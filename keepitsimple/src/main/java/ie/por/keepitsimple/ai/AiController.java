f2308a424486e9e98ede21eeca106d5c9318ba72
poraif <peadar.oraif@gmail.com>
2025-03-06 21:51:28 +0100

    initial generate term method constructed

--- keepitsimple/src/main/java/ie/por/keepitsimple/ai/AiController.java
+++ keepitsimple/src/main/java/ie/por/keepitsimple/ai/AiController.java
@@ -1,12 +1,19 @@
 package ie.por.keepitsimple.ai;
 
 import ie.por.keepitsimple.requestbody.termversion.AddTermVersionReqBody;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/ai")
 public class AiController {
 
     @Autowired
--- keepitsimple/src/main/java/ie/por/keepitsimple/ai/AiService.java
+++ keepitsimple/src/main/java/ie/por/keepitsimple/ai/AiService.java
@@ -1,15 +1,42 @@
 package ie.por.keepitsimple.ai;
 
 import ie.por.keepitsimple.requestbody.termversion.AddTermVersionReqBody;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 @Service
 public class AiService {
 
-    public AddTermVersionReqBody generateTerm(String termName) {
-        AddTermVersionReqBody reqBody = new AddTermVersionReqBody();
-        return reqBody;
+    private AddTermVersionReqBody addTermVersionReqBody;
+
+    private ChatClient chatClient;
+
+    public AiService(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
+    @Value("classpath:/prompts/generateTermPrompt")
+    private Resource generatePrompt;
+
+    @Value("classpath:/prompts/checkTermPrompt")
+    private Resource checkPrompt;
+
+    public AddTermVersionReqBody generateTerm(String term) {
+        PromptTemplate promptTemplate = new PromptTemplate(generatePrompt);
+
+        Prompt prompt = promptTemplate.create(Map.of("term", term));
+        return this.chatClient.prompt(prompt)
+                .call()
+                .entity(AddTermVersionReqBody.class);
     }
 
     public boolean checkTerm(String termName) {
